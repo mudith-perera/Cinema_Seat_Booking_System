289cb0e8bf35241c5716ca2b6209b14d602face1
mudith-perera <makawitagemudith@gmail.com>
2021-11-11 19:34:13 +0530

    commit test 2

--- src/CinemaHall.java
+++ src/CinemaHall.java
@@ -8,7 +8,7 @@ public class CinemaHall extends Application {
         GuiFunctions guiFunctions = new GuiFunctions();
         //guiFunctions.logIn();
         guiFunctions.mainMenu();
-        //comment check
+        //comment
 
     }
     public static void main(String [] cake){
